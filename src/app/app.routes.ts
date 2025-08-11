b3a7ab817aaffa8891e11c6b0406f6334b3bb50b
John Mendoza <jhoamen93@hotmail.com>
2025-08-11 11:56:34 -0500

    first configuration module federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+export const appHomeRoutes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
